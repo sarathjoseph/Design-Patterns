1f7e22af29285edae9b7981e90d4a8960ac05937
sarathjoseph <sarathcjoseph@gmail.com>
2016-04-30 11:14:26 -0400

    remove unused cpp files

--- Design Patterns/Observer/Observer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Observer.cpp
-//  Design Patterns
-//
-//  Created by SARATH CHERIYAN JOSEPH on 4/20/16.
-//  Copyright © 2016 SARATH CHERIYAN JOSEPH. All rights reserved.
-//
-
-#include "Observer.hpp"
